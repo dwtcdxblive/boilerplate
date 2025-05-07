2cf8d46c481873e8abc5929499768f616e677e6e
dwtcdxblive <omar.aouf@dwtc.com>
2025-05-07 09:17:31 +0400

    Vite - SASS - bootstrap Boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
+
